6b0f9455d7abb31ffbd0ab7ed8f5a83e70667b43
“sumitkumarjha123” <sumit.jha2@byjus.com>
2021-11-05 18:39:01 +0530

    update

---
+++ client/src/Header.js
@@ -0,0 +1,21 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <Navbar bg="dark" expand="lg" variant='dark'>
+      <Container>
+        <Link to="/" className='navbar-brand'>Meeting-App</Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" className="nav-link" active>Home</Link>
+            <Link to="/add-movie" className="nav-link">Add Movie</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header;
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+const PG_URL=process.env.PG_URL
 const Sequelize = require("Sequelize");
 
-const PG_URL = "postgres://ukdlfwca:p1kBiW9q-d_vksow3JbUQF8RKTGmKVq-@fanny.db.elephantsql.com/ukdlfwca";
+//const PG_URL = "postgres://ukdlfwca:p1kBiW9q-d_vksow3JbUQF8RKTGmKVq-@fanny.db.elephantsql.com/ukdlfwca";
 
 const sequelize = new Sequelize(PG_URL, {
   dialect: "postgres"
